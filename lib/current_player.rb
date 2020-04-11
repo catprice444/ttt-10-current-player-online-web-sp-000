d27aa7c56bc5d7feb2a01474e85c50267e43b65b
Catherine Price <catprice444@gmail.com>
2020-04-11 03:33:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,3 +14,17 @@ def current_player(board)
   else turn_count(board).odd?
     puts "X"
 end
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
